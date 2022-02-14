f6e2b3083d96d0a0d06ae7a65ff3551d357e3e1a
Sensei <Sensei>
2022-02-14 12:11:31 +0100

    Szamologep kezdes

---
+++ 01_Szamologep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_Szamologep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
